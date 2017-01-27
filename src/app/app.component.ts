eceede7530eb2a4f7fd9d2a0e5caf87ed7517888
Gerhard Van Deventer <vandeventerg@out.co.za>
2017-01-27 10:35:35 +0200

    Just mesing aroun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Inline template</h1> 
+    <app-databinding></app-databinding>
+  `,
+  styles: [`
+    h1{
+      color:red; 
+    }
+  `]
 })
 export class AppComponent {
   title = 'Now Changed.';
---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent{
+  stringInterpolation = 'This is a string.'; 
+  numberInterpolation = 8; 
+  onTest(){
+    return false;
+    
+  }
+} 
+
+
